5887133dbcf3579e64ae81b558c341c96f48c70c
Luis Henrique <duimbispo@gmail.com>
2025-08-12 10:05:32 -0300

    Criação da tabela categorias

--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -1,8 +1,18 @@
-import { Entity } from "typeorm";
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+//Criando a tabela categorias
 @Entity(
   {name: "tb_categorias"}
 )
 
+// Atributos da tabela categoria
 export class Categoria{
+  
+  @PrimaryGeneratedColumn()
+    id: number;
+
+    @IsNotEmpty()
+    @Column({length: 200, nullable: false})
+    nome_categoria: string;
 }

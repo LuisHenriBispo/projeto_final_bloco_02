490ba701292db309c1f93bd879c22bce1ddbedd2
Luis Henrique <duimbispo@gmail.com>
2025-08-12 10:31:11 -0300

    Criação da tabela produtos

--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -1,6 +1,28 @@
-import { Entity } from "typeorm";
+import { IsNotEmpty, IsNumber } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({ name: "tb_produtos" })
 export class Produto {
+  @PrimaryGeneratedColumn()
+    id: number
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    nome: string
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    descricao: string
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    fabricante: string
+
+    @UpdateDateColumn()
+    data_validade: Date;
+
+    @IsNumber()
+    @Column('decimal', { precision: 10, scale: 2, nullable: false })
+    preco: number;
     
 }

37562d74b8efbf0bd5e98291e4904fc08d95d801
Luis Henrique <duimbispo@gmail.com>
2025-08-12 13:01:22 -0300

    Implementação ProdutoService com métodos CRUD

--- src/produto/services/produto.service.ts
+++ src/produto/services/produto.service.ts
@@ -1,6 +1,75 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Produto } from "../entities/produto.entity";
+import { DeleteResult, ILike, Repository } from "typeorm";
 
 @Injectable()
 export class ProdutoService {
+
+    constructor(
+        @InjectRepository(Produto)
+        private produtoRepository: Repository<Produto>
+    ) { }
+
+     //Listar todos os produtos
+    async findAll(): Promise<Produto[]> {
+        return await this.produtoRepository.find({
+            relations: {
+                categoria: true
+            }
+        });
+    }
+
+    //Listar produtos por id
+    async findById(id: number): Promise<Produto> {
+
+        let produto = await this.produtoRepository.findOne({
+            where: {
+                id
+            },
+            relations: {
+                categoria: true
+            }
+        });
+
+        if (!produto)
+            throw new HttpException('Produto não encontrado!', HttpStatus.NOT_FOUND);
+
+        return produto;
+    }
+
+    // Listar produto por nome
+    async findAllBynome(nome: string): Promise<Produto[]> {
+        return await this.produtoRepository.find({
+            where: {
+                nome: ILike(`%${nome}%`)
+            },
+            relations: {
+                categoria: true
+            }
+        })
+    }
+
+    //Criar produto
+    async create(produto: Produto): Promise<Produto> {
+        return await this.produtoRepository.save(produto);
+    }
+
+    //Atualizar produto
+    async update(produto: Produto): Promise<Produto> {
+
+        await this.findById(produto.id);
+
+        return await this.produtoRepository.save(produto);
+    }
+
+    //Deletar produto
+    async delete(id: number): Promise<DeleteResult> {
+
+        await this.findById(id);
+
+        return await this.produtoRepository.delete(id);
+
+    }
     
 }
